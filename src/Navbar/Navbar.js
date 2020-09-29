ab8ddd9e3638bd18acc025bcb5f195a972ab575c
roneet002 <roneet.kumar002@gmail.com>
2020-09-29 12:38:23 +0530

    nav css added in folder

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,90 +1,94 @@
 import React, { Component } from 'react'
-
+import './Navbar.css'
 class Navbar extends Component{
     render(){
 return(
 
-	<nav classname="clear navbar">
 
-	<ul classname="listNone navbarLft">
+	<nav className="navbar">
 
-	<li classname="megaMenu">
-        <a href="#" classname="dropDown">Bras</a>
-	<ul classname="listNone subMenu">
-        <li classname="subMenuContent pull-left"> 
- 			  <ul classname="coslmenu listNone">
-                <li classname="no-border"><p>Shop By Style<span classname="headSp caretdrp"></span></p></li>
-	 			<li><a href="#"><span classname="newicons headSp"></span>Full Cup</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>3/4th Demi cup</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Half cup</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Plunge neck</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Balconette</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Tube bra</a></li>
+	<ul className="navbarLft">
+
+	<li className="megaMenu">
+        <a href="#" className="dropDown">Bras</a>
+	<ul className="subMenu">
+        <li className="subMenuContent"> 
+ 			  <ul className="coslmenu">
+                <li className="no-border"><p>Shop By Style</p></li>
+	 			<li><a href="#">Full Cup</a></li>
+                <li><a href="#">3/4th Demi cup</a></li>
+                <li><a href="#">Half cup</a></li>
+                <li><a href="#">Plunge neck</a></li>
+                <li><a href="#">Balconette</a></li>
+                <li><a href="#">Tube bra</a></li>
               </ul>
-              <ul classname="coslmenu listNone">
-                <li classname="no-border"> 
-                  <p>Shop By Coverage<span classname="headSp caretdrp"></span></p>
+              <ul className="coslmenu listNone">
+                <li className="no-border"> 
+                  <p>Shop By Coverage</p>
              </li>
-                <li><a href="#"><span classname="newicons headSp"></span>T Shirt</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Sports</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Push Up </a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Strapless/Multiway</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Halter-neck</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Large Size</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Bralette</a></li>
+                <li><a href="#">T Shirt</a></li>
+                <li><a href="#">Sports</a></li>
+                <li><a href="#">Push Up </a></li>
+                <li><a href="#">Strapless/Multiway</a></li>
+                <li><a href="#">Halter-neck</a></li>
+                <li><a href="#">Large Size</a></li>
+                <li><a href="#">Bralette</a></li>
               </ul>
-              <ul classname="coslmenu listNone">
-                <li classname="no-border"> 
-                  <p>Shop By Fabric<span classname="headSp caretdrp"></span></p>
+              <ul className="coslmenu listNone">
+                <li className="no-border"> 
+                  <p>Shop By Fabric </p>
                   </li>
-                <li><a href="#"><span classname="newicons headSp"></span>Backless</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Front Open</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Minimisers</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Invisible </a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Free Size</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Seamless</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Magic</a></li>
+                <li><a href="#">Backless</a></li>
+                <li><a href="#">Front Open</a></li>
+                <li><a href="#">Minimisers</a></li>
+                <li><a href="#">Invisible </a></li>
+                <li><a href="#">Free Size</a></li>
+                <li><a href="#">Seamless</a></li>
+                <li><a href="#">Magic</a></li>
               </ul>
-              <ul classname="coslmenu listNone">
-                <li classname="no-border">
-                  <p>Shop By Offers<span classname="headSp caretdrp"></span></p>
+              <ul className="coslmenu listNone">
+                <li className="no-border">
+                  <p>Shop By Offers</p>
                    </li>
-                <li><a href="#"><span classname="newicons headSp"></span>Designer Bras</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Lace Bras</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>All Bras</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>4 Bras For 499</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Bestseller</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Bra &amp; Brief Sets</a></li>
-                <li><a href="#"><span classname="newicons headSp"></span>Bridal</a></li>
+                <li><a href="#">Designer Bras</a></li>
+                <li><a href="#">Lace Bras</a></li>
+                <li><a href="#">All Bras</a></li>
+                <li><a href="#">4 Bras For 499</a></li>
+                <li><a href="#">Bestseller</a></li>
+                <li><a href="#">Bra &amp; Brief Sets</a></li>
+                <li><a href="#">Bridal</a></li>
               </ul>
             
               </li>
-			<li classname="rftImgSection pull-right">
-					<a href="#" classname="fullImg"><img classname="imgResponsive" src="https://image.clovia.com/media/images/mainmenu-picture1-jliwt-1.jpg"/></a>
+			<li className="rftImgSection">
+					<a href="#" className="fullImg"><img className="imgResponsive" src="https://image.clovia.com/media/images/mainmenu-picture1-jliwt-1.jpg"/></a>
 			</li>	
 	</ul>
 	
 	
 	</li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Panties</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Nightwear</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Clothing</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Bridal</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Gift Sets</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">4 Bras @ 699</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">5 panties @ 599</a></li>	
-	<li classname="megaMenu highLight"><a href="#" classname="dropDown">3 panties @ 599</a></li>	
-	<li classname="megaMenu highLight"><a href="#" classname="dropDown">2 Plush bras @ 999</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Shapewear</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Beach</a></li>	
-	<li classname="megaMenu"><a href="#" classname="dropDown">Club Clovia</a></li>	
-	<li classname="rftLi"><span classname="headSp SearchIcon"></span></li>
-	<li classname="rftLi"><span classname="headSp CartIcon"></span></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Panties</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Nightwear</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Clothing</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Bridal</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Gift Sets</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">4 Bras @ 699</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">5 panties @ 599</a></li>	
+	<li className="megaMenu highLight"><a href="#" className="dropDown">3 panties @ 599</a></li>	
+	<li className="megaMenu highLight"><a href="#" className="dropDown">2 Plush bras @ 999</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Shapewear</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Beach</a></li>	
+	<li className="megaMenu"><a href="#" className="dropDown">Club Clovia</a></li>	
+	<li className="rftLi"><span className="headSp SearchIcon"></span></li>
+	<li className="rftLi"><span className="headSp CartIcon"></span></li>	
 	</ul>	
 
-		<span classname="clear"></span>
+		{/* <span className="clear"></span> */}
 	</nav>
 	
+
+
+
 
 )
 
